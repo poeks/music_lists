01ed7ce3c91ebec73513fedb0fd627914d93f509
Jen Oslislo <poeksweb@gmail.com>
2014-04-16 16:58:58 -0700

    Tsort init

---
+++ tsort/app.rb
@@ -0,0 +1,65 @@
+require './env'
+
+ks = Kneescraper.new CONFIG["base_url"]
+path = CONFIG["path"]
+xpath = "//table[contains(@class,'albumlist')]/a"
+xpath = "//td"
+count = 0
+albums = {}
+num_cols = 2
+this_col = 1
+current_album, current_artist = "", ""
+
+ks.fetch path, xpath do |stuff|
+  if stuff.attributes["class"] and stuff.attributes["class"].value == 'aat'
+    a = stuff.children.first.text
+    if a.class==Nokogiri::XML::Element and a.name == "a"
+      current_artist = a.children.first
+    else
+      current_artist = a
+      puts "Unknown #{a.class} #{stuff}"
+    end
+  end
+
+  if stuff.attributes["class"] and stuff.attributes["class"].value == 'att'
+    current_album = stuff.children.first.text
+  end
+#<td class="aat"><a href="/music/kzun10.htm">Michael Jackson</a></td>
+#<td class="att">Thriller</td>
+  #puts stuff.children if this_col == 1
+  #puts stuff.children.inspect
+  #
+#   stuff.children.each do |thing|
+#
+#     puts thing.inspect
+#     puts
+#
+#   end
+
+#   href = stuff.attributes["href"].value
+#
+#   if href =~ /#{path}\/(.+)-\d+/
+#     count += 1
+#     words = $1.split(/-/)
+#     album = words.map {|word| word.capitalize}.join " "
+#
+#   end
+  #
+  if this_col == num_cols
+    this_col = 1
+    album = "#{current_artist}||#{current_album}"
+    if albums[album]
+      albums[album] += 1
+    else
+      albums[album] = 1
+    end
+    count += 1
+  else
+    this_col += 1
+  end
+
+end
+puts
+puts
+puts albums.keys
+puts "Count: #{count}"
